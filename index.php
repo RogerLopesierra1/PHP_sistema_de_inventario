24fc73acace12fa0b850352163dcee48feb9842e
Roger Lopesierra Olivera <rogerlopesierraolivera@gmail.com>
2022-12-13 21:47:44 -0500

    agregar codigo repetitivo inc/ y script responsive

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Sistema de inventario</title>
+	<?php include("./inc/head.php"); ?>
+	
 </head>
 <body>
-        
+    <?php 
+			include("./inc/navbar.php"); 
+			
+			include("./inc/script.php"); 
+		?>
 </body>
 </html>
