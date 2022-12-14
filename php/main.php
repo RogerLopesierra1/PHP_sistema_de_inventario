294c25db9b8b5f224dd2cdc407759cb4a96fdc4d
Roger Lopesierra Olivera <rogerlopesierraolivera@gmail.com>
2022-12-14 18:11:15 -0500

    php/main add funciones evitar inyeccion sql y renombrar fotos

--- php/main.php
+++ php/main.php
@@ -15,4 +15,49 @@ function verificar_datos($filtro, $cadena){
 	
 }
 
+// evitar inyeccion SQL
+// limpiar cadena de texto
+function limpiar_cadena($cadena){
+	$cadena=trim($cadena);
+	$cadena=stripslashes($cadena);
+	$cadena=str_ireplace("<script>", "", $cadena);
+	$cadena=str_ireplace("</script>", "", $cadena);
+	$cadena=str_ireplace("<script src", "", $cadena);
+	$cadena=str_ireplace("<script type=", "", $cadena);
+	$cadena=str_ireplace("SELECT * FROM", "", $cadena);
+	$cadena=str_ireplace("DELETE FROM", "", $cadena);
+	$cadena=str_ireplace("INSERT INTO", "", $cadena);
+	$cadena=str_ireplace("DROP TABLE", "", $cadena);
+	$cadena=str_ireplace("DROP DATABASE", "", $cadena);
+	$cadena=str_ireplace("TRUNCATE TABLE", "", $cadena);
+	$cadena=str_ireplace("SHOW TABLES;", "", $cadena);
+	$cadena=str_ireplace("SHOW DATABASES;", "", $cadena);
+	$cadena=str_ireplace("<?php", "", $cadena);
+	$cadena=str_ireplace("?>", "", $cadena);
+	$cadena=str_ireplace("--", "", $cadena);
+	$cadena=str_ireplace("^", "", $cadena);
+	$cadena=str_ireplace("<", "", $cadena);
+	$cadena=str_ireplace("[", "", $cadena);
+	$cadena=str_ireplace("]", "", $cadena);
+	$cadena=str_ireplace("==", "", $cadena);
+	$cadena=str_ireplace(";", "", $cadena);
+	$cadena=str_ireplace("::", "", $cadena);
+	$cadena=trim($cadena);
+	$cadena=stripslashes($cadena);
+	return $cadena;
+}
+
+// funcion renombrar fotos
+function renombrar_fotos($nombre){
+	$nombre=str_ireplace(" ", "_", $nombre);
+	$nombre=str_ireplace("/", "_", $nombre);
+	$nombre=str_ireplace("#", "_", $nombre);
+	$nombre=str_ireplace("-", "_", $nombre);
+	$nombre=str_ireplace("$", "_", $nombre);
+	$nombre=str_ireplace(".", "_", $nombre);
+	$nombre=str_ireplace(",", "_", $nombre);
+	$nombre=$nombre."_".rand(0,100);
+	return $nombre;
+}
+
 ?>
